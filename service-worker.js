c823eaf08204df51b4ad3dacb94a5dd049b3241e
nawihxmbuy-star <nawihxmbuy@gmail.com>
2026-06-17 08:25:26 +0700

    อัพเดท v16.0

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 🎯 service-worker.js (ฉบับแก้ไขและเสถียรที่สุด รองรับการทำงานออฟไลน์ 100%)
-const CACHE_NAME = 'nawee-pro-studio-v15.1';
+const CACHE_NAME = 'nawee-pro-studio-v16.0';
 
 const ASSETS_TO_CACHE = [
   './',
