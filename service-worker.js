3dc166e008a75ef7050871dc743f8fdbc8bede98
nawihxmbuy-star <nawihxmbuy@gmail.com>
2026-09-23 17:30:04 +0700

    Update service worker cache version to v31.0

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// 🎯 service-worker.js (v30.0 - Sunita PDF Studio Final Safe Edition)
-const CACHE_NAME = 'sunita-pdf-v30.0';
+// 🎯 service-worker.js (v31.0 - Sunita PDF Studio Final Safe Edition)
+const CACHE_NAME = 'sunita-pdf-v31.0';
 
 const CORE_ASSETS = [
   './',
