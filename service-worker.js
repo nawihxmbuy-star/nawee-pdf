c350b7b90b2e1b3a790da00d7b651a5a405e4f9f
nawihxmbuy-star <nawihxmbuy@gmail.com>
2026-09-25 14:12:47 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// 🎯 service-worker.js (v36.0 - Sunita PDF Studio Final Safe Edition)
-const CACHE_NAME = 'sunita-pdf-v36.0';
+// 🎯 service-worker.js (v37.0 - Sunita PDF Studio Final Safe Edition)
+const CACHE_NAME = 'sunita-pdf-v37.0';
 
 const CORE_ASSETS = [
   './',
